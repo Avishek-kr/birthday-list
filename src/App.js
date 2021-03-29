acd167b4f3d407d5db6632417089ccea397acc5e
Avishek-kr <ak251997@gmail.com>
2021-03-29 15:35:50 +0530

    added props people in list constant

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ function App() {
   const [people, setPeople] = useState(data)
   return (
   <main>
-   
+    <section className='container'>
+        <h3>{people.length} birthday today</h3>
+        <List people={people} />
+        <button onClick={()=>setPeople([])}>Clear all</button>
+    </section>
   </main>
   )
 }
